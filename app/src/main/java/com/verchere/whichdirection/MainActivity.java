8d8d319ac370d8bd35a1425b9259298f9592b3db
gegelascience <remi.verchere2@gmail.com>
2019-07-28 20:54:17 +0200

    move arrow and use gouv api for address, work only in France

--- app/src/main/java/com/verchere/whichdirection/MainActivity.java
+++ app/src/main/java/com/verchere/whichdirection/MainActivity.java
@@ -1,17 +1,9 @@
 package com.verchere.whichdirection;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,15 +16,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.List;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, ActivityCompat.OnRequestPermissionsResultCallback, View.OnClickListener {
 
-    public LocationManager locationManager;
-    private Location myLocation;
+    private double myLatitude, myLongitude, targetLatitude, targetLongitude;
     private SensorManager sensorManager;
-    private static final int REQUEST_PERMISSIONS = 1;
     Sensor magneto,accelero;
     float[] magnetoVector=new float[3];
     float[] acceleroVector=new float[3];
@@ -40,11 +38,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float orientationCoord = 0f;
     DirectionView direction;
     Button search;
-    EditText address;
+    EditText address,start;
     private CameraView cameraView;
     private boolean isCamOpened = false;
     private boolean isVisible = false;
-
+    private boolean islocation = false;
+    private boolean isTerminus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,88 +54,93 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         accelero = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         search = findViewById(R.id.search);
         address = findViewById(R.id.address);
+        start = findViewById(R.id.position);
         search.setOnClickListener(this);
-        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED ) {
-            boolean network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            boolean gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            if (gps_enabled){
-                myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            }
-            else if(network_enabled){
-                myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            } else {
-                Toast.makeText(MainActivity.this, "Network not enabled", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.CAMERA}, REQUEST_PERMISSIONS);
-        }
-        float LOCATION_REFRESH_DISTANCE = 1;
-        long LOCATION_REFRESH_TIME = 100;
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_REFRESH_TIME,
-                LOCATION_REFRESH_DISTANCE, mLocationListener);
 
     }
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            //your code here
-            myLocation = location;
-        }
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
 
     @Override
     public void onClick(View v){
         switch (v.getId()){
             case R.id.search:
-                latLngFromAddress(address.getText().toString());
+                latLngFromAddress(address.getText().toString(),start.getText().toString());
                 break;
             default:
                 break;
         }
     }
 
-    public void latLngFromAddress(String address){
-        Geocoder coder = new Geocoder(this);
-        List<Address> addressList;
-        try {
-            if (address != null && !address.equals("") && !address.equals(" ")){
-                addressList = coder.getFromLocationName(address,5);
-                if (addressList != null){
-                    Address directionAddress = addressList.get(0);
-                    double x = Math.cos(Math.toRadians(myLocation.getLatitude()))* Math.sin(Math.toRadians(directionAddress.getLatitude())) - Math.sin(Math.toRadians(directionAddress.getLatitude()))* Math.cos(Math.toRadians(myLocation.getLatitude()))* Math.cos(Math.toRadians(directionAddress.getLongitude()-myLocation.getLongitude()));
-                    double y = Math.cos(Math.toRadians(myLocation.getLatitude()))* Math.sin(Math.toRadians(directionAddress.getLongitude()-myLocation.getLongitude()));
-                    orientationCoord =(float) Math.round(Math.toDegrees(Math.atan2(y,x)));
-                    Log.e("angle direction", String.valueOf(orientationCoord));
-                    setARView();
-                } else {
-                    TextView error = findViewById(R.id.error);
+    public void latLngFromAddress(String address, String location){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String paramsDirection = address.trim().replace(" ", "+");
+        String paramsStart = location.trim().replace(" ", "+");
+        String urlDirection = "https://api-adresse.data.gouv.fr/search/?q="+ paramsDirection + "&limit=1";
+        String urlStart = "https://api-adresse.data.gouv.fr/search/?q="+ paramsStart + "&limit=1";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlDirection, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                TextView error = findViewById(R.id.error);
+                try{
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONObject result = new JSONObject(jsonResponse.getJSONArray("features").getString(0));
+                    JSONArray coordinates = result.getJSONObject("geometry").getJSONArray("coordinates");
+                    targetLatitude = coordinates.getDouble(1);
+                    targetLongitude = coordinates.getDouble(0);
+                    isTerminus = true;
+                    if (isTerminus && islocation){
+                        setARView();
+                    }
+
+                }catch (JSONException e){
                     error.setVisibility(View.VISIBLE);
                 }
-            }
 
-        }catch (IOException e){
-            Log.e("fail found address",e.getMessage());
-            TextView error = findViewById(R.id.error);
-            error.setVisibility(View.VISIBLE);
-        }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView errorT = findViewById(R.id.error);
+                errorT.setVisibility(View.VISIBLE);
+            }
+        });
+        StringRequest stringRequest2 = new StringRequest(Request.Method.GET, urlStart, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                TextView error = findViewById(R.id.error);
+                try{
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONObject result = new JSONObject(jsonResponse.getJSONArray("features").getString(0));
+                    JSONArray coordinates = result.getJSONObject("geometry").getJSONArray("coordinates");
+                    myLatitude = coordinates.getDouble(1);
+                    myLongitude = coordinates.getDouble(0);
+                    islocation = true;
+                    if (isTerminus && islocation){
+                        setARView();
+                    }
+
+                }catch (JSONException e){
+                    error.setVisibility(View.VISIBLE);
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView errorT = findViewById(R.id.error);
+                errorT.setVisibility(View.VISIBLE);
+            }
+        });
+        queue.add(stringRequest);
+        queue.add(stringRequest2);
     }
 
     public void setARView(){
+        double x = Math.cos(Math.toRadians(myLatitude))* Math.sin(Math.toRadians(targetLatitude)) - Math.sin(Math.toRadians(targetLatitude))* Math.cos(Math.toRadians(myLatitude))* Math.cos(Math.toRadians(targetLongitude-myLongitude));
+        double y = Math.cos(Math.toRadians(myLatitude))* Math.sin(Math.toRadians(targetLongitude-myLongitude));
+        orientationCoord =(float) Math.round(Math.toDegrees(Math.atan2(y,x)));
+        Log.e("angle direction", String.valueOf(orientationCoord));
         setContentView(R.layout.ar_layout);
         cameraView = new CameraView((TextureView) findViewById(R.id.view), this);
         TextureView textureView = findViewById(R.id.direction);
@@ -168,21 +172,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         azimut = (float) -Math.round(Math.toDegrees(orientation[0]));
         if (isCamOpened){
-            if (azimut> (orientationCoord-5) && azimut < (orientationCoord +5)){
-                if (!isVisible){
-                    isVisible = true;
-                    direction.setVisibility(true);
-
-                }
-
-            } else {
-                if(isVisible){
-                    isVisible = false;
-                    direction.setVisibility(false);
-
-                }
-
-            }
+            direction.mTextureView.setRotation(azimut - orientationCoord);
         }
     }
 
@@ -191,34 +181,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (sens.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             if (accuracy == 0 | accuracy == 1){
                 Toast.makeText(MainActivity.this, "Please recalibrate your magnetometer", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-    }
-
-    @Override
-    public  void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if (requestCode == REQUEST_PERMISSIONS) {
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED ) {
-                    boolean network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-                    boolean gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-                    if (gps_enabled){
-                        myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    }
-                    else if(network_enabled){
-                        myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    } else {
-                        Toast.makeText(MainActivity.this, "Network not enabled", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Permissions denied", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            } else {
-                Toast.makeText(MainActivity.this, "Permissions denied", Toast.LENGTH_SHORT).show();
-                finish();
             }
         }
 
